0bece50638fcb09c79d07b16916c77b23efb0fe5
charliebrandli <charlotte.brandli@squirro.com>
2025-12-16 11:03:05 +0100

    added code which downloads video attachments

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
 from atlassian import Confluence
+import requests
+
+
+# make sure env variables are set
+if not os.getenv("CONFLUENCE_URL"):
+    raise RuntimeError("CONFLUENCE_URL is not set")
 
 # create Confluence Client
 confluence = Confluence(
@@ -8,30 +14,38 @@ confluence = Confluence(
     password=os.getenv("CONFLUENCE_API_TOKEN"))
 
 #get page ID
-page_ID = 3354460162 # ID of Sprint Reviews 2025 file
+page_ID = 4224679955 # ID of Test Page with video attachments
 page = confluence.get_page_by_id(page_ID)
 print(f"Page title: {page['title']}")
+print()
 
 # list all attachments on the page
 attachments = confluence.get_attachments_from_content(page_ID)
+print("Attachments: ")
 for att in attachments['results']:
     print(att['title'])
+print()
 
 # filter attachments to find video files
 video_extensions = (".mp4", ".mov", ".mkv")
 
 video_attachments = [att for att in attachments['results'] if att['title'].lower().endswith(video_extensions)]
 
-print (f"found {len(video_attachments)} videos")
+print (f"Found {len(video_attachments)} videos: ")
 for video in video_attachments:
     print(video['title'])
+print()
 
 # download video files
-os.makedirs('./downloaded_videos', exist_ok=True)
+os.makedirs('./downloaded_videos', exist_ok=True) # make a directory for the downloaded videos
 
 for video in video_attachments:
-    print
-    content = confluence.get_attachment_content(video['id'])
+    print(f"Downloading {video['title']}")  
+    download_url = confluence.url + video['_links']['download']
+    response = requests.get(download_url, auth=(confluence.username, confluence.password))
+
     with open(f'./downloaded_videos/{video["title"]}', 'wb') as file:
-        file.write(content)
-    print(f"Downloaded: {video['title']}")
+        file.write(response.content)
+    print(f"Downloaded {video['title']}")
+print(f"Downloaded {len(video_attachments)} videos")
+print()
