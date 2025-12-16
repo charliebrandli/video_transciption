4b0741eb330c50676cfa1e6bd1d30e8411013489
charliebrandli <charlotte.brandli@squirro.com>
2025-12-16 13:37:04 +0100

    started working on audio transcription

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 from atlassian import Confluence
 import requests
 import subprocess
+import openai
+
+# get OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+if openai.api_key is None:
+    raise RuntimeError("OPENAI_API_KEY is not set")
 
 
 # make sure env variables are set
@@ -56,7 +62,8 @@ os.makedirs('./audio_extractions', exist_ok=True) # make a directory for the aud
 
 for video in video_attachments:
     video_file = f'./downloaded_videos/{video["title"]}'
-    audio_file = f'./audio_extractions/{video["title"].split(".")[0]}.wav' # add .wav extension
+    video_basename = video["title"].split(".")[0] # filename without extension
+    audio_file = f'./audio_extractions/{video_basename}.wav' # add .wav extension
     
     if os.path.exists(audio_file):
         print(f"Audio of this video already exists, skipping: {audio_file}")
@@ -66,7 +73,25 @@ for video in video_attachments:
     subprocess.run([
         'ffmpeg',
         '-i', video_file,
-        '-ac', '1',
+        '-ac', '1', # mono audio
         audio_file
     ], check=True)
     print("Done")
+
+# transcribe the audio
+# hardcoded for one video file only
+os.makedirs('./transcripts', exist_ok=True)
+
+audio_file = './audio_extractions/TedTalk-Karen_Faith.wav'
+with open(audio_file, "rb") as f:
+    transcript = openai.audio.transcriptions.create(
+        model="whisper-1",
+        file=f
+    )
+basename = os.path.basename(audio_file).split(".")[0]
+output_file = f"./transcripts/{basename}.txt"
+
+with open(output_file, "w", encoding="utf-8") as out:
+    out.write(transcript['text'])
+
+#print(f"Transcribing audio from {video['title']} ...")
